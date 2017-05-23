02d65ab47a4f6b0f31c7bc9be9261bc3644d5270
KEHANG <kehanghan@gmail.com>
2017-05-23 12:24:33 -0400

    write access log to file

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+import logging
+
 from kehangsite import app
+
+logger = logging.getLogger('werkzeug')
+handler = logging.FileHandler('access.log')
+logger.addHandler(handler)
 
 app.run(host="0.0.0.0", port=80, debug=True)
