54bba6376f80db7293c29907a4995c3ecfe417fd
KEHANG <kehanghan@gmail.com>
2019-04-27 21:09:38 -0400

    remove app.run which will be taken care of by launch script

--- run.py
+++ run.py
@@ -5,5 +5,3 @@ from kehangsite import app
 logger = logging.getLogger('werkzeug')
 handler = logging.FileHandler('access.log')
 logger.addHandler(handler)
-
-app.run(host="0.0.0.0", port=80, debug=True)
